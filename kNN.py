2cfa7d063aa42015f8f69f0f2fba3030093d8803
rod <rod@pu-gh.com>
2012-08-05 17:44:48 +0100

    added refactored handwriting example

--- kNN.py
+++ kNN.py
@@ -1,5 +1,7 @@
 
 from numpy import *
+from os import listdir
+
 import operator
 
 def createDataSet():
@@ -54,6 +56,10 @@ def file2matrix(filename):
         index += 1
     return returnMat, classLabelVector
 
+#
+#  improving matches from a dating site
+#
+
 def datingClassTest():
     hoRatio = 0.10
     datingDataMat, datingLabels = file2matrix( './data/ch02/datingTestSet.txt' )
@@ -66,4 +72,45 @@ def datingClassTest():
         print "the classifier came back with %s, the real answer is %s" % (classifierResult, datingLabels[i])
         if ( classifierResult != datingLabels[i] ): errorCount += 1.0
     print "the total error rate is %f" % (errorCount / float(numTestVecs))
+
+#
+#  kNN on handwritten digits
+#
+
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+def hwTrainingMat(dataDir):
+    trainingFileList = listdir( '%s/trainingDigits' % dataDir )
+    hwLabels = []
+    m = len( trainingFileList )
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[ i ]
+        fileStr = fileNameStr.split( '.' )[ 0 ]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('%s/trainingDigits/%s' % (dataDir, fileNameStr))
+    return trainingMat, hwLabels
+
+def handwritingClassTest(dataDir):
+    trainingMat, hwLabels = hwTrainingMat(dataDir)
+    testFileList = listdir('%s/testDigits' % dataDir)
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('%s/testDigits/%s' % (dataDir, fileNameStr))
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 2)
+        print "the classifier came back with: %s, the real answer is: %s" % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print "\nthe total number of errors is: %d" % errorCount
 
